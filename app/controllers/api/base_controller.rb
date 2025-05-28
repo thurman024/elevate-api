e6d0a1731bc5f2b02ad38e8a54b8309009176eee
Stephen Thurman <thurman024@gmail.com>
2025-05-28 11:39:21 -0600

    phase 2 - game event ingestion

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -3,7 +3,13 @@ module Api
     before_action :validate_session_token
 
     def validate_session_token
-      @current_user = User.find_by(session_token: session[:session_token])
+      auth_header = request.headers['Authorization']
+
+      if auth_header&.starts_with?('Token ')
+        token = auth_header.split(' ').last
+        @current_user = User.find_by(session_token: token)
+      end
+
       head :unauthorized unless @current_user
     end
   end
